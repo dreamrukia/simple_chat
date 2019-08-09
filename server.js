ed883e109e2b8cf48b5dd99f5b7dda28a9e7da6b
dreamrukia <dreamrukia@hotmail.com>
2019-08-09 00:33:30 -0500

    Finished html file and part of the server.

--- server.js
+++ server.js
@@ -54,3 +54,28 @@ var server = http.createServer(function (req, res) {
 server.listen(80, function(){
     console.log("Server listening on Port 80.");
 });
+
+var chatServer = require("./lib/chat_server");
+chatServer.listen(server);
+var socketio = require("socket.io");
+var io;
+var guestNum = 1;
+var nickNames = {};
+var namesUsed = [];
+var currRoom = {};
+
+exports.listen = function(server) {
+    io = socketio.listen(server);
+    io.set("log level", 1);
+    io.sockets.on("connection", function (socket){
+        guestNum = assignGuestName(socket, guestNum, nickNames, namesUsed);
+        joinRoom(socket, "Lobby");
+        handleMessageBroadcasting(socket, nickNames);
+        handleNameChangeAttempts(socket, nickNames, namesUsed);
+        handleRoomJoining(socket);
+        socket.on("rooms", function(){
+            socket.emit("rooms", io.sockets.manager.rooms);
+        });
+        handleClientDisconnection(socket, nickNames, namesUsed);
+    });
+};
